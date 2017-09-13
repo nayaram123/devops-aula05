e6fd6f5a2e385ce130d1f1654349d769c96170e8
nayaram123 <30813693+nayaram123@users.noreply.github.com>
2017-09-12 22:15:00 -0300

    Update jogovelha.py

--- src/jogovelha.py
+++ src/jogovelha.py
@@ -3,7 +3,7 @@ def inicializar():
     for i in range(3):
         linha = [ ]
         for j in range(3):
-            linhaappend("X")
+            linha.append("O")
         tab.append(linha)
       return tab
       
